f16f1b3fab5610f9274cff676fb3b9ab383a3b83
sstefanofm <sstefanofm@gmail.com>
2021-11-15 22:41:31 -0300

    Plane and Truck override the 'mail' method from the MailerTransport interface

--- design-patterns/FactoryMethod_Mailing/src/factory/Truck.java
+++ design-patterns/FactoryMethod_Mailing/src/factory/Truck.java
@@ -1,4 +1,9 @@
 package factory;
 
-public class Truck implements Transport {
+public class Truck implements MailerTransport {
+
+    @Override
+    public void mail() {
+        System.out.println("Honk! I'm sending you your package by ground  * Says the truck * ");
+    }
 }
