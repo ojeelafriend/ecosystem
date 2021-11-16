71f6d06df0025fc1d2b9f38262cf2d2e513fb813
sstefanofm <sstefanofm@gmail.com>
2021-11-15 22:42:21 -0300

    Tried to do the code as simple as possible

--- design-patterns/FactoryMethod_Mailing/src/factory/Main.java
+++ design-patterns/FactoryMethod_Mailing/src/factory/Main.java
@@ -2,7 +2,19 @@ package factory;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static Mail theMail;
 
+    public static void main(String[] args) {
+        // Change the following String parameter value to "Ground"/"Air"
+        initializeMail("Ground");
+        theMail.sendMail();
+    }
+
+    private static void initializeMail(String transportType) {
+        if (transportType.equals("Ground")) {
+            theMail = new GroundMail();
+        } else if (transportType.equals("Air")) {
+            theMail = new AirMail();
+        }
     }
 }
